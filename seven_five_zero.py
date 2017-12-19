4fc8c25fa85e924bb8958592e28b5dad3f2456bd
Le Minh Nhat <nhatleminh10@gmail.com>
2017-12-19 23:33:24 +1100

    Add new solution for 750 problem

--- seven_five_zero.py
+++ seven_five_zero.py
@@ -7,13 +7,14 @@ class Solution(object):
         count = 0
         rows = len(grid)
         cols = len(grid[0])
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c]:
-                    for i in range(c+1, cols):
-                        if grid[r][i]:
-                            for j in range(r+1, rows):
-                                if grid[j][c] and grid[j][i]:
-                                    count += 1
+        for i in range(rows):
+            for j in range(i+1, rows):
+                # Fix two rows, then scan through each column to find pairs of 1
+                pairs = 0
+                for c in range(cols):
+                    if grid[i][c] and grid[j][c]:
+                        pairs += 1
+                # Pick any 2 pairs
+                count += pairs*(pairs-1)/2
         return count
 
