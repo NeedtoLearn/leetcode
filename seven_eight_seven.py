f672f630910c10e429e39b9f54e44b6fbc9687ae
Le Minh Nhat <nhatleminh10@gmail.com>
2018-04-13 22:39:41 +1000

    Remove redundant code for prob 787

--- seven_eight_seven.py
+++ seven_eight_seven.py
@@ -19,17 +19,14 @@ class Solution(object):
         # distances with format (price, city, stops).
         priority_queue = PriorityQueue()
         priority_queue.put((0, src, -1))
-        # Create list to store price from src to each city
-        prices = [-1 for i in range(n)]
+        # Find cheapest price from src to dst
         while not priority_queue.empty():
             price, city, stops = priority_queue.get()
             # Only accept the flight if stops <= K
             if stops <= K:
-                # If city is destination, then we found cheapest price
+                # If city is destination, then found cheapest price
                 if city == dst:
                     return price
-                # Update price for the city
-                prices[city] = price
                 # Add neighbors of city to priority queue
                 for i in range(n):
                     if adj_mat[city][i] != -1:
